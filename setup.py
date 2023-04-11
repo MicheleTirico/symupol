93ee8f43b900f26ff8c38bfcc1cf80d50fb54024
mt_licit <michele.tirico@entpe.fr>
2023-04-11 15:22:06 +0200

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ pkgs = find_packages('src')
 
 setup_kwds = dict(
     name='symupol',
-    version="0.0.1",
+    version="0.1",
+    author="Michele Tirico",
+    author_mail="tirico.michele@outlook.com",
+    python_required="3.9.15",
     zip_safe=False,
     packages=pkgs,
     package_dir={'': 'src'},
